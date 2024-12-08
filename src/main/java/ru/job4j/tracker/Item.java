ac7e4ef20b804923677898beb703179ae2a4aba4
Yulya <ylhappy999@gmail.com>
2024-12-09 07:17:28 +1000

    Import задания “Перегрузка онструктора“ на Github [173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -19,4 +19,26 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void show() {
+        System.out.println(id);
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item(600, "Transcend");
+        item.show();
+    }
 }
