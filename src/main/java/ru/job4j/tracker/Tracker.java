647167f495ab32a3926cd24178ec6c7c56c49e05
Yulya <ylhappy999@gmail.com>
2024-12-24 01:37:38 +1000

    Import задания “Tracker - хранилище“ на Github [396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -11,15 +13,32 @@ public class Tracker {
         return item;
     }
 
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int count = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item name = items[index];
+            if (name.getName().equals(key)) {
+                result[count++] = name;
+            }
+        }
+        result = Arrays.copyOf(result, count);
+        return result;
+    }
+
     public Item findById(int id) {
-        Item rsl = null;
+        Item result = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (item.getId() == id) {
-                rsl = item;
+                result = item;
                 break;
             }
         }
-        return rsl;
+        return result;
     }
 }
