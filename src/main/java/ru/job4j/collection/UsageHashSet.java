3d3d275f42ef7cd065df4a3a1f3473a0956ee92d
Yulya <ylhappy999@gmail.com>
2025-01-24 21:57:33 +1000

    Import задания “Set, HashSet, Iterator.“ на Github [10095]

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -1,14 +1,19 @@
 package ru.job4j.collection;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class UsageHashSet {
     public static void main(String[] args) {
+        HashSet<String> names = new HashSet<>();
         HashSet autos = new HashSet<>();
+        autos.add("BMW");
         autos.add("Lada");
+        autos.add("BMW");
         autos.add("BMW");
         autos.add("Volvo");
         autos.add("Toyoto");
+        autos.add("Volvo");
         for (Object auto : autos) {
             System.out.println(auto);
         }
