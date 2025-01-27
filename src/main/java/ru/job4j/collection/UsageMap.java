43b91ca216d108046c291fd7f5089499970681db
Yulya <ylhappy999@gmail.com>
2025-01-27 19:45:51 +1000

    Import задания “Map, HashMap“ на Github [212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("michael@gmail.ru", "Michael Grant");
+        map.put("taylor@gmail.ru", "Jennifer Taylor");
+        map.put("michael@gmail.ru", "Michael Grant John");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
